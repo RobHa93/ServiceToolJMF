727e56bc9fc39ead50cd97bd8a78e04e6cfb1375
Robin Hasler <robin.hasler@bluewin.ch>
2023-11-28 18:27:54 +0100

    Ordner mit leeren funktionen gefüllt

--- index.js
+++ index.js
@@ -11,3 +11,25 @@ const server = http.createServer((req, res) => {
 server.listen(port, host, () => {
   console.log(`Server is running at ${host}:${port}`);
 });
+
+app.use(express.json());
+
+app.get("/routes/maschinen_typ", (req, res) => {
+  // Logik zum Speichern des Maschinen-Typs implementieren
+  res.send("Maschinen-Typ");
+});
+
+app.get("/routes/tags", (req, res) => {
+  // Logik zum Speichern der Tags implementieren
+  res.send("Tags");
+});
+
+app.get("/routes/notizen", (req, res) => {
+  // Logik zum Speichern der Notizen implementieren
+  res.send("Notizen");
+});
+
+app.post("/routes/bilder", (req, res) => {
+  // Logik zum Speichern der Bilde implementieren
+  res.send("Bild hochgeladen");
+});
